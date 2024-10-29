cc921cba05ba0d169e320b496c897e6c6374bf15
Salim ATALLA <salim.atalla@etu.univ-nantes.fr>
2024-10-29 18:32:53 +0100

    Remove useless classes

--- src/Formula.cpp
+++
@@ -1,9 +0,0 @@
-#include "Formula.h"
-
-// Constructor for Formula
-Formula::Formula(const std::shared_ptr<Predicate> &predicate) : predicate(predicate) {}
-
-// Returns the predicate associated with the formula
-std::shared_ptr<Predicate> Formula::getPredicate() const {
-    return predicate;
-}
--- src/Variable.cpp
+++
@@ -1,9 +0,0 @@
-#include "Variable.h"
-
-// Constructor for Variable
-Variable::Variable(const std::string &name) : name(name) {}
-
-// Returns the name of the variable
-std::string Variable::getName() const {
-    return name;
-}
